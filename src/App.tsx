1ee44ded9180615fa4ca617ae21871ac58f140c3
Robby6Strings <rausten93@gmail.com>
2024-09-18 04:27:57 +1200

    general improvements

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Landing } from "./components/Landing"
 export function App() {
   return (
     <div className="min-h-screen flex flex-col gap-20 justify-between px-10 py-20">
-      <h1 className="text-3xl md:text-5xl leading-10 font-bold text-center">
+      <h1 className="text-3xl md:text-4xl md:leading-normal font-bold text-center">
         Welcome to your Kaioken CSR app!
       </h1>
       <Counter />
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,18 +1,18 @@
 export function Landing() {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-12">
       <div className="flex flex-wrap gap-8 justify-center">
         <LandingCard
           iconSrc="/signal.svg"
-          text="global state via signals/stores"
+          text="Global state via signals/stores"
         />
         <LandingCard
           iconSrc="/route.svg"
-          text="routing straight out of the box"
+          text="Comprehensive routing & CSR utilities by default"
         />
         <LandingCard
           iconSrc="/hook.svg"
-          text="granular control of reactivity & state"
+          text="Granular control of reactivity & state"
         />
       </div>
       <div className="flex justify-center">
@@ -33,10 +33,9 @@ type LandingCardProps = {
 }
 function LandingCard({ iconSrc, text }: LandingCardProps) {
   return (
-    <div className="flex flex-col items-center text-center gap-4">
-      <img src={iconSrc} className="w-16 h-16" alt="Route" />
-      <img src="/arrowd.svg" className="animate-bounce" alt="Route" />
-      <p>{text}</p>
+    <div className="flex flex-col md:basis-1/4 flex-grow items-center text-center gap-4 rounded-xl border border-white border-opacity-5 bg-white bg-opacity-5 p-6 shadow-lg backdrop-blur-md">
+      <img src={iconSrc} className="w-12 h-12 opacity-60" alt="Route" />
+      <p className="text-lg font-thin">{text}</p>
     </div>
   )
 }
