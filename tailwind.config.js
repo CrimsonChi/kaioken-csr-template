2c7f7f41c7b95e6f0a85a515579daebf33e2020f
itsYaraso <benshigregg@gmail.com>
2024-09-17 15:37:00 +0300

    counter finalized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -17,21 +19,21 @@ export default {
             opacity: 1,
             transform: "translate3d(0, 0, 0)",
           },
-          "fade-in-right": {
+        },
+        "punch-in": {
           "0%": {
-            opacity: 0,
-            transform: "translate3d(10%, 0, 0)",
+            transform: "scale(8)",
           },
           "100%": {
-            opacity: 1,
+            transform: "scale(1)",
             transform: "translate3d(0, 0, 0)",
-          }
           },
         },
       },
+
       animation: {
         "fade-in-up": "fade-in-up .5s ease-in-out",
-        "fade-in-right": "fade-in-right .5s ease-in-out",
+        "punch-in": "punch-in .1s",
       },
     },
   },
